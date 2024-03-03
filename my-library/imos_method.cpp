b37d30cc2cabffe7a77975b772d0c7fa3107bf87
Yuulis <yuulis2005@gmail.com>
2024-03-03 20:53:21 +0900

    Updated : imos_method

--- my-library/imos_method.cpp
+++ my-library/imos_method.cpp
@@ -8,6 +8,7 @@ using namespace std;
 
 int main()
 {
+    // 1-dimension
     int N, Q;
     cin >> N >> Q;
 
@@ -25,4 +26,26 @@ int main()
     // calclate prefix_sum
     vector<int> ps(N + 1, 0);
     rep(i, 0, N) ps[i + 1] = ps[i] + v[i];
+
+    // 2-dimension
+    int H, W;
+    cin >> H >> W;
+
+    // query
+    vector<vector<int>> v2d(H, vector<int>(W, 0));
+    rep(i, 0, Q)
+    {
+        int A, B, C, D;
+        cin >> A >> B >> C >> D;
+
+        v2d[A][B]++;
+        v2d[A][D + 1]--;
+        v2d[C + 1][B]--;
+        v2d[C + 1][D + 1]++;
+    }
+
+    // calclate prefix_sum
+    vector<vector<int>> ps2d(H + 1, vector<int>(W + 1, 0));
+    rep(i, 0, H) rep(j, 0, W) ps2d[i + 1][j + 1] = ps2d[i + 1][j] + ps2d[i][j];
+    rep(j, 0, W) rep(i, 0, H) ps2d[i + 1][j + 1] = ps2d[i][j + 1] + ps2d[i + 1][j + 1];
 }
