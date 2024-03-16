52cd002b7b4bb95dd8e237c1b2181f20345555ca
Yuulis <yuulis2005@gmail.com>
2024-03-16 12:45:41 +0900

    Solved : tessoku-book/A29~31, B29~31

---
+++ tessoku-book/a30/main.cpp
@@ -0,0 +1,137 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+
+#define all(x) (x).begin(), (x).end()
+#define fix(x) fixed << setprecision(x)
+#define rep(i, start, end) for (ll i = (start); i < (ll)(end); i++)
+#define rrep(i, start, end) for (ll i = ((ll)(start)); i >= (end); i--)
+
+constexpr auto PI = 3.14159265358979;
+constexpr auto INF = 1e+9;
+constexpr auto INFL = 1LL << 60;
+
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using lld = long double;
+using mint = modint1000000007;
+// using mint = modint998244353;
+using Pair_int = pair<int, int>;
+using Pair_ll = pair<ll, ll>;
+using Grid = vector<string>;
+
+struct Edge
+{
+    int to;
+    int cost;
+};
+
+ll ceil(ll a, ll b)
+{
+    if (a % b == 0)
+        return a / b;
+    return (a / b) + 1;
+}
+/*
+mint modPow(ll x, ll n)
+{
+    mint ans = 1;
+    for (ll i = 0; i < n; i++)
+        ans *= x;
+    return ans;
+}
+*/
+ll gcd(ll x, ll y)
+{
+    if (x < y)
+        swap(x, y);
+    ll r;
+    while (y > 0)
+    {
+        r = x % y;
+        x = y;
+        y = r;
+    }
+    return x;
+}
+ll lcm(ll x, ll y) { return ll(x / gcd(x, y)) * y; }
+ll nCk(ll n, ll r)
+{
+    if (r < 0 || n < r)
+        return 0;
+    ll ans = 1;
+    for (ll i = 1; i <= r; i++)
+    {
+        ans *= n--;
+        ans /= i;
+    }
+    return ans;
+}
+int get_rand(int seed, int min, int max)
+{
+    static mt19937_64 mt64(seed);
+    uniform_int_distribution<int> get_rand_int(min, max);
+    return get_rand_int(mt64);
+}
+template <class T>
+bool chmax(T &a, const T &b)
+{
+    if (a < b)
+    {
+        a = b;
+        return 1;
+    }
+    return 0;
+}
+template <class T>
+bool chmin(T &a, const T &b)
+{
+    if (b < a)
+    {
+        a = b;
+        return 1;
+    }
+    return 0;
+}
+template <class T1, class T2>
+inline auto mod(T1 x, T2 r) { return (x % r + r) % r; }
+template <class T>
+using Graph = vector<vector<T>>;
+
+// ======================================== //
+
+const int MAX = 510000;
+mint fac[MAX], finv[MAX], inv[MAX];
+
+void nCkInit()
+{
+    const int MOD = mint::mod();
+    fac[0] = fac[1] = 1;
+    finv[0] = finv[1] = 1;
+    inv[1] = 1;
+    for (int i = 2; i < MAX; i++)
+    {
+        fac[i] = fac[i - 1] * i;
+        inv[i] = MOD - inv[MOD % i] * (MOD / i);
+        finv[i] = finv[i - 1] * inv[i];
+    }
+}
+
+mint nCk_mod(ll n, ll k)
+{
+    if (n < k)
+        return 0;
+    if (n < 0 || k < 0)
+        return 0;
+    return fac[n] * finv[k] * finv[n - k];
+}
+
+int main()
+{
+    nCkInit();
+
+    int n, r;
+    cin >> n >> r;
+
+    cout << nCk_mod(n, r).val() << endl;
+}
