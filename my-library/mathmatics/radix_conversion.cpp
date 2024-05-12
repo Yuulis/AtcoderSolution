aa0fb832c7ab6050bce3be581e7a42f33298c91d
Yuulis <yuulis2005@gmail.com>
2024-05-13 00:40:43 +0900

    Updated : radix_conversion

--- my-library/mathmatics/radix_conversion.cpp
+++ my-library/mathmatics/radix_conversion.cpp
@@ -1,76 +1,47 @@
-#include <bits/stdc++.h>
-using namespace std;
-typedef long long ll;
-
-#define rep(i, start, end) for (ll i = (start); i < (ll)(end); i++)
+#include <iostream>
+#include <bitset>
+#include <string>
+#include <cassert>
+#include <sstream>
 
 int main()
 {
     // decimal -> binary (filled with eight 0s)
     unsigned x = 0;
-    cout << static_cast<std::bitset<8>>(x) << endl;
+    std::cout << static_cast<std::bitset<8>>(x) << std::endl;
 
     // Example
-    cout << static_cast<std::bitset<8>>(10) << endl; // 00001010
+    std::cout << static_cast<std::bitset<8>>(10) << std::endl; // 00001010
 }
 
-// k-base num -> decimal
-int ktoDec(ll x, ll k)
+// Arbitrary radix conversion
+std::string radix_conversion(const std::string x, const int from, const int to)
 {
-    ll res = 0;
-    ll base = 1;
-    while (x > 0)
+    // Conversion table.
+    // To be changed as appropriate.
+    const std::string table = "0123456789abcdef";
+    assert((2 <= from && from <= 16) && (2 <= to && to <= 16));
+
+    unsigned long sum = 0;
+    for (char c : x)
     {
-        res = res + (x % 10) * base;
-        x = x / 10;
-        base = base * k;
+        sum = sum * from + table.find(c);
     }
+
+    std::string res = "";
+    do
+    {
+        int mod = sum % to;
+        res = table[mod] + res;
+        sum /= to;
+    } while (sum);
 
     return res;
 }
-// Example
-int x = ktoDec(101, 4); // x = 17
 
-// decimal -> k-base num
-int dectoK(ll x, ll k)
+// Example
+int main()
 {
-    string s;
-    while (x != 0)
-    {
-        s += to_string(x % k);
-        x /= k;
-    }
-
-    int res = atoi(s.c_str());
-    return res;
+    std::string x = "110";
+    std::cout << radix_conversion(x, 2, 10) << std::endl; // 6
 }
-// Example
-int x = dectoK(10, 4); // x = 22
-
-// decimal -> hexadecimal (specifiable length)
-string dectoh(int x, int size = 0)
-{
-    char hex_buf[16];
-    sprintf(hex_buf, "%X", x);
-    string hex_str = hex_buf;
-    if (size == 0)
-    {
-        return hex_str;
-    }
-
-    string front;
-    if (x >= 0)
-        front = "0";
-    else
-        front = "F";
-
-    int diff = size - hex_str.length();
-    rep(i, 0, diff) hex_str = front + hex_str;
-
-    if (diff < 0)
-        hex_str.erase(0, -diff);
-
-    return hex_str;
-}
-// Example
-string s = dectoh(26, 3); // s = "01A"
