b0a13047f610dac8bbb480913bd2cd53efd6c69f
Yuulis <yuulis2005@gmail.com>
2024-08-11 20:44:26 +0900

    Updated : prefix_sum.cpp

--- my-library/prefix_sum.cpp
+++ my-library/prefix_sum.cpp
@@ -7,25 +7,36 @@ int main()
 {
     int N, M;
     cin >> N >> M;
-    vector<int> A(N);
+    vector<int> A(N, 0);
     rep(i, 0, N) cin >> A[i];
-    vector<vector<int>> B(N, vector<int>(M));
-    rep(i, 0, N) rep(j, 0, M) cin >> B[i][j];
+    vector<vector<int>> B(N, vector<int>(N, 0));
+    rep(i, 0, N) rep(j, 0, N) cin >> B[i][j];
+    vector<vector<vector<int>>> C(N, vector<vector<int>>(N, vector<int>(N, 0)));
+    rep(i, 0, N) rep(j, 0, N) rep(k, 0, N) cin >> C[i][j][k];
 
-    // 1-dimension
+    // When given in a closed interval, subtract 1 from the left end.
+
+    // ===== 1-dimension ===== //
     vector<int> ps(N + 1, 0);
     rep(i, 0, N) ps[i + 1] = ps[i] + A[i];
 
-    // sum of [3, 8)
-    cout << ps[8] - ps[3] << endl;
+    // sum of [x1, x2)
+    int x1, x2;
+    cout << ps[x2] - ps[x1] << endl;
 
-    // 2-dimension
-    vector<vector<int>> ps2d(N + 1, vector<int>(M + 1, 0));
-    rep(i, 0, N) rep(j, 0, M) ps2d[i + 1][j + 1] = ps2d[i + 1][j] + ps2d[i][j + 1] - ps2d[i][j] + B[i][j];
+    // ===== 2-dimension ===== //
+    vector<vector<int>> ps2d(N + 1, vector<int>(N + 1, 0));
+    rep(i, 0, N) rep(j, 0, N) ps2d[i + 1][j + 1] = ps2d[i][j + 1] + ps2d[i + 1][j] - ps2d[i][j] + B[i][j];
 
-    rep(i, 0, N) rep(j, 0, M) ps2d[i + 1][j + 1] = ps2d[i + 1][j] + B[i][j];
-    rep(i, 0, N) rep(j, 0, M) ps2d[i + 1][j + 1] = ps2d[i][j + 1] + ps2d[i + 1][j + 1];
+    // sum of [x1, y1) * [x2, y2)
+    int y1, y2;
+    cout << ps2d[x2][y2] - ps2d[x1][y2] - ps2d[x2][y1] + ps2d[x1][y1] << endl;
 
-    // sum of [2, 5) * [4, 7)
-    cout << ps2d[8][7] - ps2d[2][7] - ps2d[5][4] + ps2d[2][4] << endl;
+    // ===== 3-dimension ===== //
+    vector<vector<vector<int>>> ps3d(N + 1, vector<vector<int>>(N + 1, vector<int>(N + 1, 0)));
+    rep(i, 0, N) rep(j, 0, N) rep(k, 0, N) ps3d[i + 1][j + 1][k + 1] = ps3d[i][j + 1][k + 1] + ps3d[i + 1][j][k + 1] + ps3d[i + 1][j + 1][k] - ps3d[i][j][k + 1] - ps3d[i][j + 1][k] - ps3d[i + 1][j][k] + ps3d[i][j][k] + C[i][j][k];
+
+    // sum of [x1, y1, z1) * [x2, y2, z2)
+    int z1, z2;
+    cout << ps3d[x2][y2][z2] - ps3d[x1][y2][z2] - ps3d[x2][y1][z2] - ps3d[x2][y2][z1] + ps3d[x1][y1][z2] + ps3d[x1][y2][z1] + ps3d[x2][y1][z1] - ps3d[x1][y1][z1] << endl;
 }
